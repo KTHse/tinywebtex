ade2ef4071fa55e831a09af35130581b1856c337
Fredrik Jönsson <fjo@kth.se>
2012-11-15 19:17:06 +0100

    More styling, make textarea adapt to width. Put stuff in top-down order to decrease complexity. Started on hidden tables for buttons.

--- tinywebtex/langs/en_dlg_src.js
+++ tinywebtex/langs/en_dlg_src.js
@@ -1,5 +1,7 @@
 tinyMCE.addI18n('en.tinywebtex_dlg',{
-	title : 'Equation Editors',
-	done : 'Done',
-	charsLeft : 'Number of characters left before the image link becomes too long to show properly in some browsers.'
+    charsLeft : 'Number of characters left before the image link becomes too long to show properly in some browsers.',
+    done : 'Done',
+    editExpression : 'Edit mathematical expression (TeX)',
+    help : 'Help',
+    title : 'Equation Editor'
 });
--- tinywebtex/langs/sv_dlg_src.js
+++ tinywebtex/langs/sv_dlg_src.js
@@ -1,5 +1,7 @@
 tinyMCE.addI18n('sv.tinywebtex_dlg',{
-	title : 'Ekvationseditor',
+    charsLeft : 'Tecken kvar innan bildl&auml;nken blir f&ouml;r l&aring;ng f&ouml;r att visas i vissa webbl&auml;sare.',
     done : 'Klar',
-    charsLeft : 'Tecken kvar innan bildl&auml;nken blir f&ouml;r l&aring;ng f&ouml;r att visas i vissa webbl&auml;sare.'
+    editExpression : 'Redigera matematiskt uttryck (TeX)',
+    help : 'Hj&auml;lp',
+    title : 'Ekvationseditor'
 });
