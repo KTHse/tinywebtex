79dcd0bc07c628485c364bc3ba99577d34c9c260
Fredrik Jönsson <fjo@kth.se>
2012-11-19 19:13:10 +0100

    IE seemed to break, try another way.

--- tinywebtex/editor_plugin_src.js
+++ tinywebtex/editor_plugin_src.js
@@ -26,9 +26,8 @@
             ed.addCommand('mceTinyWebtex', function() {
                 if (ed.dom.getAttrib(ed.selection.getNode(), 'class', '').indexOf('mceItem') != -1)
                     return;
-                
-                ed.selection.setContent('<span id="tw_stupid_ie_workaround">' + ed.selection.getContent()+ '</span>');
-                ed.selection.select(ed.dom.get('tw_stupid_ie_workaround'));
+
+                ed.selection.setContent('<span id="tw_stupid_ie_workaround">' + ed.selection.getContent() + '</span>');
 
                 ed.windowManager.open({
                     file : url + '/dialog.htm',
