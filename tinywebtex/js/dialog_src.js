43da36fa84a6b26896b171628cad6e1181f8323b
Fredrik Jönsson <fjo@kth.se>
2012-11-22 07:58:03 +0100

    Fix initialization isssue in IE8, work remains.

--- tinywebtex/js/dialog_src.js
+++ tinywebtex/js/dialog_src.js
@@ -303,7 +303,7 @@ var TinyWebtexDialog = {
 
     
     closeMenues: function() {
-        var menues = document.getElementsByClassName("twMenuPane"),
+        var menues = document.querySelectorAll(".twMenuPane"),
             i;
             
         for (i = 0; i < menues.length; i++) {
@@ -314,9 +314,9 @@ var TinyWebtexDialog = {
 
     initShortcuts : function() {
         var tw = this,
-            entries = document.getElementsByClassName("twMenuEntry"), 
-            menues = document.getElementsByClassName("twMenu"),
-            panes = document.getElementsByClassName("twMenuPane"),
+            entries = document.querySelectorAll(".twMenuEntry"), 
+            menues = document.querySelectorAll(".twMenu"),
+            panes = document.querySelectorAll(".twMenuPane"),
             i;
             
         for (i = 0; i < panes.length; i++) {
@@ -333,7 +333,6 @@ var TinyWebtexDialog = {
                 tw.insertAtCursor(this.title);
                 tw.update(); 
                 document.forms[0].tex.focus();           
-                return false;
             };          
         }
         for (i = 0; i < menues.length; i++) {
@@ -345,7 +344,6 @@ var TinyWebtexDialog = {
                     tw.closeMenues();
                     el.style.display = "block";
                 }
-                return false;
             };
         }
     },
