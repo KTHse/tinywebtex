d1a96a18ebb04cfb14f6c0b5c94501ecc1b80efd
Fredrik Jönsson <fjo@kth.se>
2012-12-04 11:48:51 +0100

    Use 'close' rather than 'done'.

--- tinywebtex/langs/en_dlg_src.js
+++ tinywebtex/langs/en_dlg_src.js
@@ -12,7 +12,7 @@ tinyMCE.addI18n('en.tinywebtex_dlg',{
     arrowsetc : 'Arrows/Relations',
     charsLeft : 'Number of characters left before the image link becomes too long to show properly in some browsers.',
     display: 'Display',
-    done : 'Done',
+    done : 'Close',
     doneTitle : 'Close window (esc)',
     editExpression : 'Edit mathematical expression (TeX)',
     functions : 'Func.',
--- tinywebtex/langs/sv_dlg_src.js
+++ tinywebtex/langs/sv_dlg_src.js
@@ -12,7 +12,7 @@ tinyMCE.addI18n('sv.tinywebtex_dlg',{
     arrowsetc : 'Pilar/Relationer',
     charsLeft : 'Tecken kvar innan bildl&auml;nken blir f&ouml;r l&aring;ng f&ouml;r att visas i vissa webbl&auml;sare.',
     display : 'Display',
-    done : 'Klar',
+    done : 'St&auml;ng',
     doneTitle : 'St&auml;ng f&ouml;nstret (esc)',
     editExpression : 'Redigera matematiskt uttryck (TeX)',
     functions : 'Funk.',
